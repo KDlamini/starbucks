d5b652c2e2c46a0078782fa2f50f68c1b4554533
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-04-28 12:05:58 +0200

    added responsive design media query

--- src/script.js
+++ src/script.js
@@ -7,3 +7,12 @@ const imageSlider = (currentImg) => {
 const changeCircleColor = (color) => {
     document.querySelector('.circle').style.background = color;
 }
+
+//Handle menu button
+const toggleMenu = () => {
+    const menuToggle = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation');
+
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
